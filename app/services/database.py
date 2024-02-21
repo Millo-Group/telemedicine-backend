8700f8d61db186be55b283d716de58033c081623
Lokesh <lokesh@75way.com>
2024-02-21 12:25:01 +0530

    Updated gitignore

--- app/services/database.py
+++ app/services/database.py
@@ -4,6 +4,6 @@ from os.path import abspath
 
 def init_db():
     db = False
-    db = firestore.Client.from_service_account_json(abspath('app/google-config.json'))
+    db = firestore.Client.from_service_account_json(abspath('google-config.json'))
     print(f"DB initialized {db.project}")
     return db
